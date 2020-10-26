725fa72a6b687ba07543385729b4b89f9f7d2e54
Pat5657 <43613117+pat5657@users.noreply.github.com>
2020-10-26 02:48:53 +0000

    Added success view and started styling it.

--- app/src/main/java/com/example/a16057851/MainActivity.java
+++ app/src/main/java/com/example/a16057851/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.a16057851;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,9 +24,10 @@ public class MainActivity extends AppCompatActivity {
 
     // Define valA + valB = validAnswer
     private int valA, valB, validAnswer;
+    private ImageView[] starViews = new ImageView[5];
     // Define views
     private TextView questionField;
-    private View successView;
+    private ConstraintLayout successLayout;
     // Define number cap to which answers and number can reach.
     private static int NUMBER_CAP = 9;
 
@@ -25,9 +36,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Define question field
-        questionField = findViewById(R.id.txtQuestion);
-        // Define success view
-        successView = findViewById(R.id.successView);
+        this.questionField = findViewById(R.id.txtQuestion);
+        // Define success layout view
+        this.successLayout = findViewById(R.id.successLayout);
         // Generate first question
         this.createQuestion();
     }
@@ -75,16 +86,114 @@ public class MainActivity extends AppCompatActivity {
         int num = this.getIntFromImageView((ImageView) v);
         // Compare selected number and valida answer
         boolean result = num == this.validAnswer;
-        // If correct, get next question
+        // If correct, display success screen
         if (result) {
-            outputField.setText(R.string.correct);
-            // Display success screen
-            // this.successView.setVisibility(View.VISIBLE);
-            // Get next question
-            createQuestion();
+            this.displaySuccessView();
         } else { // Else, display incorrect message
             outputField.setText(R.string.incorrect);
         }
+    }
+
+    /**
+     * Handle displaying success view after a successful answer.
+     */
+    private void displaySuccessView() {
+        // Define answer field
+        TextView answerField = findViewById(R.id.txtAnswer);
+        // Display answer
+        answerField.setText(String.format("%d + %d = %d", this.valA, this.valB, this.validAnswer));
+        // Display stars
+        for (int i = 0; i < 5; i++) {
+            ImageView star = this.loadStars();
+            this.starViews[i] = star;
+        }
+        // Bring replay button and answer field to front
+        findViewById(R.id.txtAnswer).bringToFront();
+        findViewById(R.id.btnReplay).bringToFront();
+        // Display success screen
+        this.successLayout.setVisibility(View.VISIBLE);
+    }
+
+    private ImageView loadStars() {
+        // Define constraint set
+        ConstraintSet cSet = new ConstraintSet();
+        // Define star image view
+        ImageView star = new ImageView(this);
+        // Set star image
+        star.setImageResource(R.drawable.star_kids);
+        // Set Id
+        star.setId(this.genViewId());
+        int starId = star.getId();
+        // Add star to the layout
+        this.successLayout.addView(star);
+        // Set constraints
+        cSet.clone(this.successLayout);
+        cSet.connect(starId, ConstraintSet.TOP, this.successLayout.getId(), ConstraintSet.TOP, this.getRandomNumber(50, 1400));
+        cSet.connect(starId, ConstraintSet.LEFT, this.successLayout.getId(), ConstraintSet.LEFT, this.getRandomNumber(50, 800));
+        cSet.applyTo(this.successLayout);
+        // Define random offset for animation start
+        int offsetStart = this.getRandomNumber(0, 2000);
+        // Define animation set
+        AnimationSet animationSet = new AnimationSet(false);
+        animationSet.setStartOffset(offsetStart);
+        // Add scale animations
+        ScaleAnimation scale = new ScaleAnimation(0, 1, 0, 1);
+        scale.setDuration(5000);
+        scale.setRepeatCount(Animation.INFINITE);
+        animationSet.addAnimation(scale);
+        // Add rotate animation
+        RotateAnimation rotate = new RotateAnimation(0, 360);
+        rotate.setDuration(5000);
+        rotate.setRepeatCount(Animation.INFINITE);
+        animationSet.addAnimation(rotate);
+        // Add translate animation
+        TranslateAnimation trans = new TranslateAnimation(0, 200, 0, 100);
+        trans.setDuration(5000);
+        trans.setRepeatCount(Animation.INFINITE);
+        animationSet.addAnimation(trans);
+        // Add fade out animation
+        AlphaAnimation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setStartOffset(2500);
+        fadeOut.setDuration(2500);
+        fadeOut.setRepeatCount(Animation.INFINITE);
+        animationSet.addAnimation(fadeOut);
+        // Apply animation set
+        star.startAnimation(animationSet);
+        // Return star
+        return star;
+    }
+
+    private int getRandomNumber(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
+    }
+
+    private int genViewId() {
+        int result;
+        // Define random generator
+        Random r = new Random();
+        // Generate new valid view id
+        do {
+            result = r.nextInt(255);
+        } while (findViewById(result) != null);
+        // Return id
+        return result;
+    }
+
+    /**
+     * Close success view and get new question.
+     * @param v View
+     */
+    public void replay(View v) {
+        // Generate new question
+        this.createQuestion();
+        // Remove stars
+        for (int i = 0; i < this.starViews.length; i++) {
+            this.starViews[i].clearAnimation();
+            this.successLayout.removeView(this.starViews[i]);
+        }
+        // Hide success screen
+        this.successLayout.setVisibility(View.INVISIBLE);
     }
 
     /**
